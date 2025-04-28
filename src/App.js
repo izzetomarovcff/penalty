85d20da9d09ef7631fa2f84a6a7ebdcf664b1754
unknown <izzetomarovcff@gmail.com>
2025-04-29 02:34:49 +0400

    askkasfkasf

--- src/App.js
+++ src/App.js
@@ -1,77 +1,27 @@
-import logo from './logo.svg';
-import "bootstrap/dist/js/bootstrap.min.js";
-import './App.css';
-import Login from './pages/Login';
-import {Route, Routes } from 'react-router-dom';
-import Accessdenied from './pages/Accessdenied';
-import PrivateRoute from './privateroutes/PrivateRoute';
-import PrivateRouteHome from './privateroutes/PrivateRouteHome';
-import Home from './pages/Home';
-import Penaltysettings from './pages/Penalty/Penaltysettings';
-import Penaltygame from './pages/Penalty/Penaltygame';
-import Minessettings from './pages/Mines/Minessettings';
-import Minesgame from './pages/Mines/Minesgame';
-import Minesrequests from './pages/Mines/Minesrequests';
-import Penaltyrequests from './pages/Penalty/Penaltyrequests';
-import Aviatorsettings from './pages/Aviator/Aviatorsettings';
-import Aviatorgame from './pages/Aviator/Aviatorgame';
-import Aviatorrequests from './pages/Aviator/Aviatorrequests';
-import Mrthimblesettings from './pages/Mrthimble/Mrthimblesettings';
-import Mrthimblegame from './pages/Mrthimble/Mrthimblegame';
-import Mrthimblerequests from './pages/Mrthimble/Mrthimblerequests';
-import Highflyersettings from './pages/Highflyer/Highflyersettings';
-import Highflyergame from './pages/Highflyer/Highflyergame';
-import Highflyerrequests from './pages/Highflyer/Highflyerrequests';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { IsLogIn } from './Redux/actions';
+// server.js
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
 
+// Set storage to /video
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '/video'); // Upload to /video
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + '-' + file.originalname;
+    cb(null, uniqueName);
+  }
+});
 
-function App() {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    const is_login = localStorage.getItem('is_login')
-    if(is_login){
-      dispatch(IsLogIn(true))
-    }
-    
-  },[])
+const upload = multer({ storage });
 
-  return (
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path='/login' element={<Login />} />
-        </Route>
+app.post('/upload', upload.single('file'), (req, res) => {
+  console.log('Uploaded:', req.file.path);
+  res.send('File uploaded successfully.');
+});
 
-        <Route element={<PrivateRouteHome />}>
-          <Route path='/' element={<Home/>}/>
-
-          <Route path='/penalty/settings' element={<Penaltysettings/>}/>
-          <Route path='/penalty/play' element={<Penaltygame/>}/>
-          <Route path='/penalty/requests' element={<Penaltyrequests/>}/>
-          
-          <Route path='/mines/settings' element={<Minessettings/>}/>
-          <Route path='/mines/play' element={<Minesgame/>}/>
-          <Route path='/mines/requests' element={<Minesrequests/>}/>
-
-          <Route path='/aviator/settings' element={<Aviatorsettings/>}/>
-          <Route path='/aviator/play' element={<Aviatorgame/>}/>
-          <Route path='/aviator/requests' element={<Aviatorrequests/>}/>
-
-          <Route path='/mrthimble/settings' element={<Mrthimblesettings/>}/>
-          <Route path='/mrthimble/play' element={<Mrthimblegame/>}/>
-          <Route path='/mrthimble/requests' element={<Mrthimblerequests/>}/>
-
-          <Route path='/highflyer/settings' element={<Highflyersettings/>}/>
-          <Route path='/highflyer/play' element={<Highflyergame/>}/>
-          <Route path='/highflyer/requests' element={<Highflyerrequests/>}/>
-
-
-        </Route>
-        <Route path='*' element={<Accessdenied/>}/>
-      </Routes>
-    
-  );
-}
-
-export default App;
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
